fffaa9726ed60cdafe038c72ebc7757ae9f4bc3f
mrLazyGuy <mrlazyguy33@gmail.com>
2025-05-29 18:40:55 +0545

    Update server.js

--- server.js
+++ server.js
@@ -3,4 +3,5 @@ const app = require('./app')
 app.listen('4000',()=>{console.log("4000 port on listening")});
 
 
-console.log("MY NAME IS MR MAINALY KSHITIZ mainaly")
+console.log("MY NAME IS MR MAINALY KSHITIZ mainaly and my name is mr lazy nice to meet you kshitiz");
+
